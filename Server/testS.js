2aafc778e858325c407d702c8c640eb5ba32d206
Olaf Maas <olaf.maas@gmail.com>
2014-05-12 10:41:29 +0200

    Server met clientLijst

--- Server/testS.js
+++ Server/testS.js
@@ -1,7 +1,31 @@
 var io = require('socket.io').listen(1337)
+var clientList = {};
+var idList = new Array();
+
 
 io.sockets.on('connection', function (socket) {
+  
+  clientList[socket.id] = socket;
+  idList.push(socket.id);
+
+  //Get data and send it back.
   socket.on('getPoint', function (data) {
   	socket.emit('getPoint',data);
   });
+
+  //Return clientList to client
+  socket.on('getClients', function (data){
+  	socket.emit('clientList', idList);
+  })
+
+  //Send message to client x
+  socket.on('sendToClient', function (data){
+  	clientList[data.id].emit('messageFromClient', {id: socket.id, message: data});
+  })
+
+  //Handle Disconnet
+  socket.on('disconnect', function (data){
+  	delete clientList[socket.id];
+  	idList.splice(idList.indexOf(socket.id), 1);
+  })
 });
