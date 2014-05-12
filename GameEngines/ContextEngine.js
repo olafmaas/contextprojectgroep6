cd4e29534ffd31e47819a8ca4ac8f21283916195
Killya8D <beerdrift@gmail.com>
2014-05-12 16:11:31 +0200

    Folder ge-renamend

---
+++ GameEngines/ContextEngine.js
@@ -0,0 +1,82 @@
+function Game(load, update, draw){
+	var width = document.body.clientWidth;
+	var height = document.body.clientHeight;
+	var backGroundColor = "#000000";
+
+	parentUpdate = function(){
+		updateGameDimensions();
+
+		update();
+
+		parentDraw();
+	}
+
+	parentDraw = function(){
+		var ctx = canv.getContext("2d");
+
+		clearCanvas();
+		draw(ctx);
+
+	}
+
+	createCanvas = function(){
+		var canv = document.createElement("canvas");
+		canv.id = 'gameCanvas';
+
+		canv.width = width;
+		canv.height = height;
+
+		document.body.appendChild(canv);
+
+		return document.getElementById("gameCanvas");
+	}
+
+	updateGameDimensions = function(){
+		width=window.innerWidth
+		|| document.documentElement.clientWidth
+		|| document.body.clientWidth;
+
+		height=window.innerHeight
+		|| document.documentElement.clientHeight
+		|| document.body.clientHeight;
+
+		canv.width = width;
+		canv.height = height;
+	}
+
+	clearCanvas = function(){
+		var ctx = canv.getContext("2d");
+		ctx.clearRect(0, 0, width, height);
+		ctx.fillStyle = backGroundColor;
+		ctx.fillRect(0, 0, width, height);
+	}
+
+	Initialize = function(){
+		canv = createCanvas();
+	}
+
+	Boot = function(){
+		load();
+		setInterval(parentUpdate, 17);
+	}
+
+	Initialize();
+	Boot();
+}
+
+function Sprite(){
+	var texture;
+	var position = {x: 0, y: 0};
+	var origin = {x: 0, y: 0};
+	var rotation = 0;
+	var scale = {x: 1, y: 1};
+
+	LoadContent = function(texturePath){
+		this.texture = new Image();
+		this.texture.src = texturePath;
+	}
+
+	Draw = function(canvasContext){
+		canvasContext.drawImage(this.texture, position.x, position.y);
+	}
+}
