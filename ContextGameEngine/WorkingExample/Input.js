e279b8a6bc2c57d63753d5aef5359da51570b213
Killya8D <beerdrift@gmail.com>
2014-05-16 11:36:08 +0200

    Working example geupdate

--- ContextGameEngine/WorkingExample/Input.js
+++ ContextGameEngine/WorkingExample/Input.js
@@ -1,14 +1,25 @@
-InputManager = function(){
-	this.mainPointer = {isActive: true, x: 0, y: 0};
+var input = {
+	mainPointer: {isActive: true, x: 0, y: 0},
 
-	this.Update = function(){
+	/**
+	* The update function for updating the input
+	* 
+	* @method Update
+	*/
+	update: function(){
 		//TODO: update main pointer door muis of touchscreen
 		
-		console.log(this.mainPointer);
-	};
+		//console.log(this.mainPointer);
+	},
 
-	this.MouseMoveListener = function(e){
-		this.mainPointer = {isActive: true, x: e.clientX, y: e.clientY};
-		console.log(this.mainPointer);
-	};
+	mouseMoveListener: function(_e){
+		this.mainPointer = {isActive: true, x: _e.clientX, y: _e.clientY};
+		//console.log(this.mainPointer);
+
+		mouseX = this.mainPointer.x;
+		mouseY = this.mainPointer.y;
+	}
 }
+
+var mouseX = 0;
+var mouseY = 0;
--- ContextGameEngine/WorkingExample/Shield.js
+++ ContextGameEngine/WorkingExample/Shield.js
@@ -7,24 +7,44 @@
 //	The pole for which the shield is destined
 function Shield(_pole){
 	//Shield properties
-	this.pole = _pole; //the pole which the shield protects
-	this.angle = 0; //the angle of the shield
-	this.position = {x: _pole.getPosition().x, y: _pole.getPosition().y};
-	this.radius = 30; //the radius of the shield
-	this.size = 0.5 * Math.PI; //The size of the shield
-	this.color = "white"; //Color of the shield
+	var pole = _pole; //the pole which the shield protects
+	var angle = 0; //the angle of the shield
+	var position = {x: _pole.getPosition().x, y: _pole.getPosition().y};
+	var radius = 70; //the radius of the shield
+	var size = 0.5 * Math.PI; //The size of the shield
+	var color = "white"; //Color of the shield
 
 	//Draws the shield
-	this.Draw = function (_canvasContext){
+	this.draw = function (_canvasContext){
 		//Draw the first half of the shield
 		_canvasContext.beginPath();
-  		_canvasContext.arc(this.position.x, this.position.y, this.radius, this.angle - (this.size / 2), this.angle + (this.size / 2));
-  		_canvasContext.strokeStyle = this.color;
+  		_canvasContext.arc(position.x, position.y, radius, angle - (size / 2), angle + (size / 2));
+  		_canvasContext.strokeStyle = color;
   		_canvasContext.stroke();
 	}
 
-	this.Update = function(){
-		this.angle = this.calculateAngle();
+	this.update = function(){
+		angle = this.calculateAngle();
+	}
+
+	/**
+	* A method that checks if the shield collides with a ball
+	* 
+	* @method collidesWith
+	* @param{_ball} The ball that should be hittested with the shield
+	*/
+	this.collidesWith = function(_ball){
+		var delta = {x: _ball.getPosition().x - position.x, y: _ball.getPosition().y - position.y};
+		var dist = _ball.getRadius() + radius;
+
+		if(Math.pow(delta.x, 2) + Math.pow(delta.y, 2) < Math.pow(dist, 2)) return this.preciseCollidesWith(delta);
+	}
+
+	this.preciseCollidesWith = function(_delta){
+		var shieldEnds = {begin: this.getAngle() - this.getSize() / 2, end: this.getAngle() + this.getSize() / 2};
+		var collisionAngle = Math.atan2(_delta.y, _delta.x);
+
+		return shieldEnds.begin < collisionAngle && shieldEnds.end > collisionAngle;
 	}
 
 	//Calculates the angle (in radian) of the shield depending on the mouse input 
@@ -33,36 +53,52 @@ function Shield(_pole){
 	//@Param _mouseX
 	//	The x coordinate of the mouse on the canvas
 	this.calculateAngle = function (){
-		return Math.atan2(game.getInput().mainPointer.y - this.position.y, game.getInput().mainPointer.x - this.position.x);
+		return Math.atan2(mouseY - position.y, mouseX - position.x);
 	}
 
 	//Sets the angle of the shield in radian
 	//@Param _angle
 	//	The specified angle 
 	this.setAngle = function (_angle){
-		this.angle = _angle;
+		angle = _angle;
 	}
 
 	//Sets the radius of the shield
 	//@Param _radius
 	//	The radius of the shield
 	this.setRadius = function (_radius){
-		this.radius = _radius;
+		radius = _radius;
+	}
+
+	this.setColor = function(_color){
+		color = _color;
+	}
+
+	this.setSize = function(_size){
+		size = _size;
 	}
 
 	//Gets the pole belonging to the shield
 	this.getPole = function (){
-		return this.pole;
+		return pole;
 	}
 
 	//Gets the current angle of the shield
 	this.getAngle = function (){
-		return this.angle;
+		return angle;
 	}
 
 	//Gets the radius of the shield
 	this.getRadius = function (){
-		return this.radius;
+		return radius;
+	}
+
+	this.getColor = function(){
+		return color;
+	}
+
+	this.getSize = function(){
+		return size;
 	}
 }
 
