2ee7d05056334bbad9a3a57a74d20dc124ed8fad
Olaf Maas <olaf.maas@gmail.com>
2014-05-20 12:30:35 +0200

    Nog wat tests

--- Testing/browsertest/circularBodyTest.js
+++ Testing/browsertest/circularBodyTest.js
@@ -15,8 +15,24 @@ describe("CircularBody", function(){
 	})
 
 	describe("#CollidesWith", function(){
-		it("Should detect a collision with an other ball")
-		it("Should throw an exception if an collision with an other object is detected")
+		var b = new Ball(10);
+
+		
+
+		it("Should detect if the other object is a ball and pass it to another function", function(){
+			var othercb = new CircularBody(b);
+			cb = new CircularBody(b);
+			var stubbedHandleBall = sinon.stub(cb, 'handleBallCollision')
+			cb.handleCollision(othercb);
+			expect(stubbedHandleBall).to.have.been.called;
+		})
+
+		it("Should detect if the other object is a shield and pass it to another function", function(){
+			cb = new CircularBody(b);
+			var stubbedHandleBall = sinon.stub(cb, 'handleShieldCollision');
+			cb.handleCollision(sinon.createStubInstance(ShieldBody));
+			expect(stubbedHandleBall).to.have.been.called;
+		})
 	})
 
 	describe("#CollidesWithBall", function(){
