3df792c740eea323a106b39b258deeb8d63a0d73
Nils <Nils_Bijlsma@hotmail.com>
2014-05-23 12:15:37 +0200

    Added colors and tested.

--- Paaltjesvoetbal/game/ColorGenerator.js
+++ Paaltjesvoetbal/game/ColorGenerator.js
@@ -5,9 +5,10 @@
 */
 var ColorGenerator = {
 	
-	colors: ["#988CFF", "#FF8C8C", "#8CE4FF", "#90FF8C", "#F4FF8C", "#FFBC8C", "#FF8CF5"],
+	colors: ["#988CFF", "#FF8C8C", "#8CE4FF", "#90FF8C", "#F4FF8C", "#FFBC8C", 
+	"#FF8CF5", "#FF4040", "#FF40DC", "#4640FF", "#29FFFB", "#38FF49", "#F5FF38", "#FF1717"],
 	
 	returnColor: function(){
-		return colors[parseInt(Math.random()*colors.length)];
+		return this.colors[parseInt(Math.random()*this.colors.length)];
 	}
 }
