f7d47e8aeb29ee719a44f50bacdd876f87500fbb
Nils <Nils_Bijlsma@hotmail.com>
2014-05-23 15:39:50 +0200

    Documentation for Color Generator and Group classes.

--- Paaltjesvoetbal/game/Group.js
+++ Paaltjesvoetbal/game/Group.js
@@ -1,6 +1,10 @@
-
-//TOOD: documentation
-//TODO: For now it's just for balls.
+/**
+* Group class
+* @class Group
+* @classdesc Class to represent objects as groups.
+* @constructor
+* @param {Object} _type - The type of objects which are stored in the group.
+*/
 function Group(_type){
 
 	//Variables
@@ -9,36 +13,66 @@ function Group(_type){
 	var collision = []; //Array for the collisions, as there could be more than 1 collision to handle per group member.
 
 
-	//Add an object to the group
+	/**
+	* Adds an object to an existing list of members
+	* @method Group#addMember
+	* @param {Object} _object - The object which is added to a list of members
+	*/
 	this.addMember = function(_object){
 		if(_object instanceof type)
 			members.push(_object);
 	}
 
+	/**
+	* Updates al objects within a list of members
+	* @method Group#update
+	*/
 	this.update = function(){
 		members.forEach(function (_object){
 			_object.update();
 		});
 	}
 
+	/**
+	* Draws all objects within a list of members
+	* @method Group#draw
+	*/
 	this.draw = function(_canvasContext){
 		members.forEach(function (_object){
 			_object.draw(_canvasContext);
 		});
 	}
 
+	/**
+	* Returns a list of members
+	* @method Group#getMembers
+	*/
 	this.getMembers = function(){
 		return members;
 	}
 
+	/**
+	* Returns a specific object from a list of members
+	* @method Group@getMember
+	* @param {int} _index - The index of the object you want to return
+	*/
 	this.getMember = function(_index){
 		return members[_index];
 	}
 
+	/**
+	* Returns the amount of objects in the list
+	* @method Group#getMembersLength
+	*/
 	this.getMemberLength = function(){
 		return members.length;
 	}
 
+	/**
+	* Checks the world bounds for every ball object
+	* @method Group#checkWorldBounds
+	* @param {Game} _game - Game object supplies the boundaries
+	*/
 	this.checkWorldBounds = function(_game){
 		if(type == Ball) { //Only check world bounds if it's a ball
 			members.forEach(function (_object){
@@ -47,6 +81,14 @@ function Group(_type){
 		}
 	}
 
+	/**
+	* Adds the correct colission between all the objects of two lists
+	* @method Group#addCollision
+	* @param {Object} _object1 - Object 1 for the possible collision
+	* @param {Object} _object2 - Object 2 for the possible collision, can be a list
+	* @param {Function} _funcAfter - 
+	* @param {Object} _this -
+	*/
 	this.addCollision = function(_object1, _object2, _funcAfter, _this){
 		//In case the second object is a group
 		if(_object2 instanceof Group){
@@ -64,6 +106,10 @@ function Group(_type){
 		}
 	}
 
+	/**
+	* Checks each collision of the possible collisions
+	* @method Group#checkCollision
+	*/
 	this.checkCollision = function(){
 		collision.forEach(function (_coll) {
 			_coll.execute();
@@ -71,6 +117,11 @@ function Group(_type){
 	}
 
 	//takes one group in which everything with everything is combined.
+	/**
+	* Adds collision between the objects of the same group (is used in the balls group)
+	* @method Group#addCollisionCombineAll
+	* @param {Group} _objectGroup - List of objects
+	*/
 	this.addCollisionCombineAll = function(_objectGroup){
 		var length = _objectGroup.getMemberLength();
 		var members = _objectGroup.getMembers();
@@ -81,5 +132,8 @@ function Group(_type){
 			}
 		}
 	}
+}
 
+if(typeof module != 'undefined'){
+    module.exports = Group;
 }
