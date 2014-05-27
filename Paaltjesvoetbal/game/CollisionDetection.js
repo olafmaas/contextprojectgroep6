28149b9e80adfa17e0d44b3968d16e073f135894
Nils <Nils_Bijlsma@hotmail.com>
2014-05-27 12:01:53 +0200

    Documentation collision detection

--- Paaltjesvoetbal/game/CollisionDetection.js
+++ Paaltjesvoetbal/game/CollisionDetection.js
@@ -3,9 +3,20 @@ if(typeof module != 'undefined'){
     var CircularBody = require('./CircularBody.js');
 }  
 
+/**
+* CollisionDetection Class
+* @class CollisionDetection
+* @classdesc Class which detects collisions and calls methods to handle them.
+*/
 var CollisionDetection = {
 	handledCollisions: [],
 
+	/**
+	* Calls basic collision function with the two objects
+	* @method CollisionDetection#handleCollision
+	* @param {object} _bodyOne - Object 1
+	* @param {object} _bodyTwo - Object 2
+	*/
 	handleCollision: function(_bodyOne, _bodyTwo){
 		if(!this.shouldHandle(_bodyOne, _bodyTwo)) return false;
 
@@ -15,6 +26,12 @@ var CollisionDetection = {
 		return true;
 	},
 
+	/**
+	* Gets the collisions between two objects
+	* @method CollisionDetection#shouldHandle
+	* @param {object} _bodyOne - Object 1
+	* @param {object} _bodyTwo - Object 2
+	*/
 	shouldHandle: function(_bodyOne, _bodyTwo){
 		//Get the collisions
 		var collisionObject = this.checkForCollision(_bodyOne, _bodyTwo);
@@ -25,12 +42,18 @@ var CollisionDetection = {
 		//Check if the collision has been handled already
 		if(this.handledCollisions.indexOf(collisionObject) > -1) return false;
 
-		//Add the collision ot handledcollisions
+		//Add the collision to handledcollisions
 		this.handledCollisions.push(collisionObject);
 
 		return true;
 	},
 
+	/**
+	* Checks for collision and returns the collision object
+	* @method CollisionDetection#checkForCollision
+	* @param {object} _bodyOne - Object 1
+	* @param {object} _bodyTwo - Object 2
+	*/
 	checkForCollision: function(_bodyOne, _bodyTwo){
 		var collisionObject = _bodyOne.ID + "with" + _bodyTwo.ID;
 
@@ -45,21 +68,45 @@ var CollisionDetection = {
 		return collisionObject;
 	},
 
+	/**
+	* Determines which collision could occur and calls the specific check method
+	* @method CollisionDetection#collides
+	* @param {object} _bodyOne - Object 1
+	* @param {object} _bodyTwo - Object 2
+	*/
 	collides: function(_bodyOne, _bodyTwo){
 		if(_bodyOne instanceof CircularBody) return this.collidesBallWith(_bodyOne, _bodyTwo);
 		if(_bodyOne instanceof ShieldBody) return this.collidesShieldWith(_bodyOne, _bodyTwo);
 	},
 
+	/**
+	* Specific collision method for ball and another object
+	* @method CollisionDetection#collidesBallWith
+	* @param {shield} _ball - Ball object
+	* @param {object} _other - Other object
+	*/
 	collidesBallWith: function(_ball, _other){
 		if(_other instanceof CircularBody) return this.collidesBallWithBall(_ball, _other);
 		if(_other instanceof ShieldBody) return this.collidesBallWithShield(_ball, _other);
 	},
 
+	/**
+	* Specific collision method for shield and another object
+	* @method CollisionDetection#collidesShieldWith
+	* @param {shield} _shield - Shield object
+	* @param {object} _other - Other object
+	*/
 	collidesShieldWith: function(_shield, _other){
 		if(_other instanceof CircularBody) return this.collidesBallWithShield(_other, _shield);
 		if(_other instanceof ShieldBody) throw "This Collision is not Implemented";
 	},
 
+	/**
+	* Specific collision method for ball and ball
+	* @method CollisionDetection#collidesBallWithBall
+	* @param {ball} _ball1 - Ball object
+	* @param {ball} _ball2 - Ball object
+	*/
 	collidesBallWithBall: function(_ball1, _ball2){
 		//Get x and y difference
 		var dx = _ball1.getPosition().x - _ball2.getPosition().x;
@@ -72,6 +119,12 @@ var CollisionDetection = {
 		return (distanceApart <= _ball1.radius + _ball2.getRadius());
 	},
 
+	/**
+	* Specific collision method for ball and shield (circle to circle collision)
+	* @method CollisionDetection#collidesBallWithShield
+	* @param {ball} _ball - Ball object
+	* @param {shield} _shield - Shield object
+	*/
 	collidesBallWithShield: function(_ball, _shield){
 		var delta = {x: _ball.getPosition().x - _shield.getPosition().x, y: _ball.getPosition().y - _shield.getPosition().y};
 		var distsq = Math.pow(delta.x, 2) + Math.pow(delta.y, 2);
@@ -83,6 +136,12 @@ var CollisionDetection = {
 		}
 	},
 
+	/**
+	* Precise collision method for ball and shield (circle to arc)
+	* @method CollisionDetection#collidesBallWithBall
+	* @param {ball} _ball - Ball object
+	* @param {ball} _shield - Shield object
+	*/
 	preciseCollidesBallWithShield: function(_ball, _shield){
 		var shieldEnds = {
 			begin: normalizeAngle(_shield.getAngle() - _shield.getParentShield().getSize() / 2), 
@@ -100,10 +159,21 @@ var CollisionDetection = {
 	}
 }
 
+/**
+* Normalizes the angle
+* @method CollisionDetection#normalizeAngle
+* @param {float} _angle - The angle of the object
+*/
 normalizeAngle = function(_angle){
 	return (_angle % (Math.PI * 2) + (Math.PI * 2)) % (Math.PI * 2);
 }
 
+/**
+* Calls the handling method for two given objects
+* @method CollisionDetection#handleCollision
+* @param {object} _objectOne - Object 1
+* @param {object} _objectTwo - Object 2
+*/
 handleCollision = function(_objectOne, _objectTwo){
 	return CollisionDetection.handleCollision(_objectOne.getBody(), _objectTwo.getBody());
 }
