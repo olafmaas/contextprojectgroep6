a85288d07960ec51c79aedfe59e95f7de15969fc
Michel <j.c.m.kraaijeveld@student.tudelft.nl>
2014-06-02 18:22:22 +0200

    Updated timer.js and added player/powerup timers
    Timer.js is now a var, as it should be able to extend this class for
    players and powerup.

---
+++ Paaltjesvoetbal/game/PlayerTimer.js
@@ -0,0 +1,35 @@
+
+if(typeof module != 'undefined'){
+	var Timer = require('./Timer.js');
+}
+
+/**
+* PlayerTimer class
+* @class PlayerTimer
+* @classdesc Class which handles the timers used for the scores in the game.
+* @constructor
+* @param {Player} _player - The player to which the timer belongs
+*/
+var PlayerTimer = Timer.extend ({
+
+	player: null,
+
+	constructor: function(_player){
+		this.player = _player;
+	},
+
+	/**
+	* Adds one to the current time and update the player's score
+	*
+	* @method PlayerTimer#count
+	*/
+	count: function(){
+		this.time++;
+		this.player.incrementScore(1);
+	}
+
+});
+
+if(typeof module != 'undefined'){
+	module.exports = PlayerTimer;
+}
---
+++ Paaltjesvoetbal/game/PowerupTimer.js
@@ -0,0 +1,38 @@
+
+if(typeof module != 'undefined'){
+	var Timer = require('./Timer.js');
+}
+
+/**
+* PowerupTimer class
+* @class PowerupTimer
+* @classdesc Class which handles the timers used for the powerups in the game. It counts down instead of up!
+* @constructor
+* @param {Powerup} _powerup - The powerup to which the timer belongs
+*/
+var PowerupTimer = Timer.extend ({
+
+	powerup: null,
+
+	constructor: function(_powerup, _time){
+		this.powerup = _powerup;
+		this.time = _time.
+	},
+
+	/**
+	* Counts one down from the initial time for the powerup. When the timer reaches 0, it is stopped
+	*
+	* @method PowerupTimer#count
+	*/
+	count: function(){
+		if(this.time > 0)
+			this.time--;
+		else 
+			this.stop();
+	}
+
+});
+
+if(typeof module != 'undefined'){
+	module.exports = PlayerTimer;
+}
