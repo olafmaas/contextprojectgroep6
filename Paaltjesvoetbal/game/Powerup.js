7eb0aef5e820de893e35bc51e126be19c51f01fc
Nils <Nils_Bijlsma@hotmail.com>
2014-06-02 16:54:39 +0200

    Extended Powerup class

--- Paaltjesvoetbal/game/Powerup.js
+++ Paaltjesvoetbal/game/Powerup.js
@@ -1,5 +1,6 @@
 if(typeof module != 'undefined'){
 	var Base = require('./Base.js');
+	var CircularBody = require('./CircularBody.js');
 	var IDDistributor = require('./IDDistributor.js');
 }
 
@@ -7,6 +8,7 @@ var Powerup = Base.extend({
 
 	body: false,
 	type: null,
+	color: "#000000",
 	radius: 1,
 	ID: -1,
 
@@ -14,9 +16,58 @@ var Powerup = Base.extend({
 		this.radius = _radius;
 		this.type = _type;
 		this.ID = IDDistributor.getNewId();
+		
+		this.enableBody();
+	},
+
+	draw: function (_canvasContext){
+		_canvasContext.beginPath();
+		_canvasContext.arc(this.getBody().position.x, this.getBody().position.y, this.radius, 0, Math.PI*2, true);
+		_canvasContext.closePath();
+		
+		_canvasContext.fillStyle = this.color;
+		_canvasContext.fill();
+	},
+	
+	equals: function(_other){
+		return (this.ID == _other.getID());
+	},
+	
+	enableBody: function(){
+		this.body = new CircularBody(this);
+	},
+	
+	collidesWith: function(_other){
+		return this.body.CollidesWith(_other);
+	},
+	
+	//==================
+	//SECTION: Get & sets
+	
+	setColor: function(_color){
+		this.color = _color;
+	},
+	
+	getColor: function(){
+		return this.color;
+	},
+	
+	getPosition: function(){
+		return this.getBody().getPosition();
+	},
+	
+	getRadius: function (){
+		return this.radius;
+	},
+	
+	getID: function(){
+		return this.ID;
+	},
+	
+	getBody: function(){
+		return this.body;
 	}
-
-
+	
 });
 
 if(typeof module != 'undefined'){
