f136f9c1b6dfbf70be839ca02f8e7fe1c41bd36c
Michel <j.c.m.kraaijeveld@student.tudelft.nl>
2014-06-02 17:05:33 +0200

    Added failing test for changing power up type

--- Testing/browsertest/powerupTest.js
+++ Testing/browsertest/powerupTest.js
@@ -2,7 +2,7 @@ describe("Powerup", function(){
 
 	describe("#Create", function(){
 
-		var p = new Powerup(10, 0)
+		var p = new Powerup(10, 0);
 
 		it("Should set the radius of this Powerup to be 10 and type to be 0", function() {
 			expect(p.getRadius()).to.equal(10);
@@ -13,7 +13,13 @@ describe("Powerup", function(){
 
 
 	describe("Type", function(){
-		//type veranderen en opvragen
+		var p = new Powerup(10, 0);
+
+		it("Should change the powerup-type from 0 to 1", function() {
+			expect(p.getType()).to.equal(0);
+			p.setType(1);
+			expect(p.getType()).to.equal(1);
+		})
 	})
 
 	describe("Timer", function(){
