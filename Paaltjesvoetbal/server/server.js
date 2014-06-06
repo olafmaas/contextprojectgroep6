1cb3083d63e0c67e4b6b863c5bf943c50ab95342
Kees Lampe <kees.lampe@gmail.com>
2014-06-06 13:08:48 +0200

    log weer op 2 gezet

--- Paaltjesvoetbal/server/server.js
+++ Paaltjesvoetbal/server/server.js
@@ -1,5 +1,5 @@
 var io = require('socket.io').listen(5050);
-io.set('log level', 3);   // 0 - error | 1 - warn | 2 - info | 3 - debug
+io.set('log level', 2);   // 0 - error | 1 - warn | 2 - info | 3 - debug
 
 var SocketHandler = require('./lib/SocketHandler.js');
 var Server = require('./lib/ServerGame.js');
