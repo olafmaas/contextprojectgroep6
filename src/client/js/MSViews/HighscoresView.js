06824f6ac599bb38090a4a64c179bde4bf07cf81
Killya8D <beerdrift@gmail.com>
2014-06-14 13:33:58 +0200

    stubbed data weggehaald

--- src/client/js/MSViews/HighscoresView.js
+++ src/client/js/MSViews/HighscoresView.js
@@ -15,9 +15,6 @@ HighscoresView = function(){
 	};
 
 	this.initialize = function(){
-		var scores = this.stubScores();
-		this.updateScores(scores);
-
 		$("#HighscoresView").css("background-color", ViewSettings.backgroundColor);
 		$("#HighscoresView").css("color", ViewSettings.textColor);
 		$("#HighscoresView").css("width", ViewSettings.viewWidth);
@@ -42,17 +39,6 @@ HighscoresView = function(){
 
 	this.updateHeight = function(){
 		$("#HighscoresView").css("height", $("#gameCanvas").css("height"));
-	}
-
-	this.stubScores = function(){
-		return [
-			{Name: "Bas", Score: 1337},
-			{Name: "Bram", Score: 1234},
-			{Name: "Bert", Score: 1000},
-			{Name: "Bart", Score: 666},
-			{Name: "Boris", Score: 127},
-			{Name: "Berend", Score: 1}
-		];
 	}
 
 	this.newScoreDiv = function(_position, _score){
