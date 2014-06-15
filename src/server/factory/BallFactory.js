00db2719c5e3d2d10453bd5b0bf9f80099b0276f
Michel <j.c.m.kraaijeveld@student.tudelft.nl>
2014-06-15 14:13:32 +0200

    Created helper functions in ballfactory

--- src/server/factory/BallFactory.js
+++ src/server/factory/BallFactory.js
@@ -11,10 +11,8 @@ if(typeof module != 'undefined'){
 var BallFactory = Base.extend({
 
 	createNewBall: function(_radius){
-		var velocity = Math.floor(Math.random() * (S.ball.velocityRange.to - S.ball.velocityRange.from+1)) + S.ball.velocityRange.from;		
-		var randomX = Math.floor(Math.random() * S.ball.nrOfNewBalls)
-		var randomY = Math.floor(Math.random() * S.ball.nrOfNewBalls)
-		var position = {x: S.ball.x + (randomX * _radius * 2), y: S.ball.y + (randomY * _radius * 2)};
+		var velocity = this.randomVelocity();
+		var position = this.randomPosition();
 
 		ball = new Ball(_radius);
 		ball.setPosition(position.x, position.y);
@@ -26,6 +24,20 @@ var BallFactory = Base.extend({
 		console.log("Velocity: " + velocity);
 		console.log("Position: " + ball.getPosition().x + " | " + ball.getPosition().y)
 		return ball;
+	},
+
+	//Returns a random velocity depending on the velocity range in the settings.
+	randomVelocity: function(){
+		return Math.floor(Math.random() * (S.ball.velocityRange.to - S.ball.velocityRange.from+1)) + S.ball.velocityRange.from;		
+	},
+
+	//Returns a random position centered around the initial position in the settings.
+	//The positions are (2 * ball radius) apart from eachother to prevent them from spawning half into another ball
+	randomPosition: function() {
+		var randomX = Math.floor(Math.random() * S.ball.nrOfNewBalls)
+		var randomY = Math.floor(Math.random() * S.ball.nrOfNewBalls)
+		
+		return {x: S.ball.x + (randomX * S.ball.size * 2), y: S.ball.y + (randomY * S.ball.size * 2)};
 	}
 });
 
